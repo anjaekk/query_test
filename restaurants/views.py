fce2f5d8b6732c35b890c76dc171b8d3a56e23ed
anjaekk <anjaekk@gmail.com>
2021-08-31 01:17:46 +0900

    Add: eager-laoding 시도

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,13 +1,34 @@
-# from django.shortcuts import render
-
-# # Create your views here.
-
+from restaurants.models import *
+from django.db import connection
 
 
-# from restaurants.models import *
+# selecte_related 미사용
+restaurant = Restaurant.objects.get(id=1)
+owner = restaurant.owner
+city = owner.city
+print(connection.queries)
 
-# for owner in Owner.objects.all():
-#     print("Execute querry")
-#     print(owner.restaurant.place)
 
-# for 
+# select_related 사용
+restaurant = Restaurant.objects.select_related('owner__city').get(id=1)
+owner = restaurant.owner
+city = owner.city
+print(connection.queries)
+
+
+# prefetch_related 미사용
+restaurants = Restaurant.objects.all()
+for restaurant in restaurants:
+    for pizza in restaurant.pizzas.all():
+        print(restaurant.name+": "+pizza.name)
+    print("")
+print(connection.queries)
+
+
+# prefetch_related 사용
+restaurants = Restaurant.objects.all().prefetch_related('pizzas')
+for restaurant in restaurants:
+    for pizza in restaurant.pizzas.all():
+        print(restaurant.name+": "+pizza.name)
+    print("")
+print(connection.queries)
